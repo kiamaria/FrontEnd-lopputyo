f5b8dda8fff486980d6cbbc72c87698a5e12666f
kiamaria <kia@hietamaki.org>
2023-11-29 21:16:08 +0200

    Osa 2

--- src/components/Customerlist.jsx
+++ src/components/Customerlist.jsx
@@ -1,50 +1,214 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-theme-material.css";
 import "ag-grid-community/styles/ag-grid.css";
-
-import moment from 'moment';
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { Input } from "@mui/base/Input";
 
 export default function Customerlist() {
+  const [customers, setCustomers] = useState([]);
+  const [open, setOpen] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [modify, setModify] = useState();
+  const [customerId, setCustomerId] = useState();
 
-    const [customers, setCustomers] = useState([]);
+  useEffect(() => fetchCustomers, []);
 
-    useEffect(() => fetchCustomers, []);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const fetchCustomers = () => {
-        fetch('http://traineeapp.azurewebsites.net/api/customers')
-        .then(response => response.json())
-        .then(data => setCustomers(data.content))
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    marginTop: 10,
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const fetchCustomers = () => {
+    fetch("http://traineeapp.azurewebsites.net/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data.content));
+  };
+
+  const addNew = async () => {
+    if (!firstName && !lastName) {
+      return;
     }
 
-    const list = [
-        {
-          field: "firstname",
-          sortable: true,
-          filter: true
+    let data = {
+      firstname: firstName,
+      lastname: lastName,
+    };
+
+    console.log("data", data);
+
+    try {
+      await fetch("https://traineeapp.azurewebsites.net/api/customers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          field: "lastname",
-          sortable: true,
-          filter: true
-        }
-      ];
-      
-    
-      console.log(customers);
-    
-      return (
-        <>
-    <div
-      className="ag-theme-material"
-      style={{ height: "700px", width: "70%", margin: "auto" }}
-    >
-      <AgGridReact 
-      columnDefs={list} 
-      rowData={customers}>
-      </AgGridReact>
-    </div>
-    </>
-      );
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      fetchCustomers();
+      handleClose();
+      resetModal();
     }
-    
+  };
+
+  const startUpdateCustomer = async (customer) => {
+    console.log(customer);
+
+    setModify(true);
+
+    setFirstName(customer.firstname);
+    setLastName(customer.lastname);
+    setCustomerId(customer.links.find((link) => link.rel === "customer").href);
+
+    handleOpen();
+  };
+
+  const updateCustomer = async () => {
+    let data = {
+      firstname: firstName,
+      lastname: lastName,
+    };
+
+    try {
+      await fetch(customerId, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setModify(false);
+      fetchCustomers();
+      handleClose();
+      resetModal();
+    }
+  };
+
+  const resetModal = async () => {
+    setFirstName();
+    setCustomerId();
+    setLastName();
+  };
+
+  const handleAdd = async () => {
+    setModify(false);
+
+    handleOpen();
+  };
+
+  const onDelClick = async (customer) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this customer?"
+    );
+    if (confirmed) {
+      try {
+        await fetch(customer.links.find((i) => i.rel === "self").href, {
+          method: "DELETE",
+        });
+
+        fetchCustomers();
+      } catch (error) {
+        console.error("Error deleting training:", error);
+      }
+    }
+  };
+
+  const list = [
+    {
+      field: "firstname",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "lastname",
+      sortable: true,
+      filter: true,
+    },
+    {
+      field: "Update",
+      sortable: false,
+      filter: false,
+      cellRenderer: (row) => {
+        return (
+          <Button onClick={() => startUpdateCustomer(row.data)}>Update</Button>
+        );
+      },
+    },
+    {
+      field: "",
+      headerName: "Delete",
+      sortable: false,
+      filterable: false,
+      cellRenderer: (row) => {
+        return <Button onClick={() => onDelClick(row.data)}>Delete</Button>;
+      },
+    },
+  ];
+
+  console.log(customers);
+
+  return (
+    <>
+      <div
+        className="ag-theme-material"
+        style={{ height: "700px", width: "70%", margin: "auto" }}
+      >
+        <h1>Customers</h1>
+        <Button onClick={handleAdd}>Add</Button>
+        <AgGridReact
+          rowHeight={30}
+          columnDefs={list}
+          rowData={customers}
+        ></AgGridReact>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Input
+              placeholder="Firstname"
+              label="Required"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+            />
+            <Input
+              placeholder="Lastname"
+              label="Required"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+            />
+            <Button onClick={modify ? updateCustomer : addNew}>Save</Button>
+          </Box>
+        </Modal>
+      </div>
+    </>
+  );
+}
--- src/components/Trainingslist.jsx
+++ src/components/Trainingslist.jsx
@@ -1,41 +1,140 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-theme-material.css";
 import "ag-grid-community/styles/ag-grid.css";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { Input } from "@mui/base/Input";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import 'dayjs/locale/fi';
 
 import moment from "moment";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  marginTop: 10,
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function Traininglist() {
   const [trainings, setTrainings] = useState([]);
-
-  const gridRef = useRef();
+  const [open, setOpen] = useState();
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [duration, setDuration] = useState();
+  const [activity, setActivity] = useState();
+  const [customerName, setCustomerName] = useState();
+  const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
     fetchTrainings();
   }, []);
 
+  useEffect(() => fetchCustomers, []);
+
+  const fetchCustomers = () => {
+    fetch("http://traineeapp.azurewebsites.net/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data.content));
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   const fetchTrainings = async () => {
+
     let trainings = await fetch(
-      "http://traineeapp.azurewebsites.net/api/trainings"
+      "https://traineeapp.azurewebsites.net/api/trainings"
     );
+
     let trainingData = await trainings.json();
 
     let trainingWithCustomers = [];
 
     for (let i = 0; i < trainingData.content.length; i++) {
       let training = trainingData.content[i];
+
       let customerResponse = await fetch(
         training.links.find((link) => link.rel === "customer").href
       );
 
       let customer = await customerResponse.json();
-      
-      training.customerName = customer.firstname + " " + customer.lastname;
 
-      trainingWithCustomers.push(training);
+      training.customerName = customer.firstname + " " + customer.lastname;
+      training.customerId = customer.id;
+      training.profile = trainingWithCustomers.push(training);
     }
 
     setTrainings(trainingWithCustomers);
+  };
+
+  const onDelClick = async (training) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this customer?"
+    );
+    if(confirmed){
+    try {
+      await fetch(training.href, {
+        method: "DELETE",
+      });
+
+      fetchTrainings();
+    } catch (error) {
+      console.error("Error deleting training:", error);
+    }
+  }};
+
+  const addNew = async () => {
+    if (
+      !date &&
+      !time &&
+      !duration &&
+      !activity &&
+      !activity &&
+      !customerName
+    ) {
+      return;
+    }
+
+    let data = {
+      date: date.toISOString(),
+      activity: activity,
+      duration: duration,
+      customer: customerName,
+    };
+
+
+    try {
+      await fetch("https://traineeapp.azurewebsites.net/api/trainings", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      fetchTrainings();
+      handleClose();
+    }
   };
 
   const list = [
@@ -72,15 +171,79 @@ export default function Traininglist() {
       sortable: true,
       filter: true,
     },
+    {
+      field: "",
+      headerName: "Delete",
+      sortable: false,
+      filterable: false,
+      cellRenderer: (row) => {
+        return (
+          <Button
+            onClick={() =>
+              onDelClick(row.data.links.find((link) => link.rel === "training"))
+            }
+          >
+            Delete
+          </Button>
+        );
+      },
+    },
   ];
 
   return (
     <>
       <div
         className="ag-theme-material"
-        style={{ height: "700px", width: "70%", margin: "auto" }}
+        style={{ height: "1000px", width: "70%", margin: "auto" }}
       >
-        <AgGridReact columnDefs={list} rowData={trainings}></AgGridReact>
+        <h1>Trainings</h1>
+        <Button onClick={handleOpen}>Add</Button>
+        <AgGridReact rowHeight={30} columnDefs={list} rowData={trainings}></AgGridReact>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fi">
+                <DateTimePicker
+                  label="Controlled picker"
+                  value={date}
+                  onChange={(newDate) => setDate(newDate)}
+                />
+            </LocalizationProvider>
+            <Input 
+              placeholder="Duration"
+              onChange={(e) => setDuration(e.target.value)}
+              value={duration}
+            />
+            <Input
+              placeholder="Activity"
+              onChange={(e) => setActivity(e.target.value)}
+              value={activity}
+            />
+            <Select
+              onChange={(e) => setCustomerName(e.target.value)}
+              value={customerName}
+            >
+              {customers.map((customer) => {
+                return (
+                  <MenuItem
+                    key={customer.id}
+                    value={
+                      customer.links.find((f) => f.rel === "customer").href
+                    }
+                  >
+                    {customer.firstname + " " + customer.lastname}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            <Button onClick={addNew}>Add</Button>
+          </Box>
+        </Modal>
       </div>
     </>
   );
